d0626d19552c273b2c165e1a5dae42a3ab108a11
Micho3 <18600775043@163.com>
2018-01-26 14:33:46 +0800

    修改练习树的生成以及点击跳转功能

--- Public/Resume/js/conf.js
+++
@@ -1,8 +0,0 @@
-    var LL_LIST = [{title:'Episode 1',data:['控制div属性1','网页换肤','函数接收参数','div变色','鼠标移入改变样式','记住密码提示框'],show:false},
-        {title:'Episode 2',data:['控制div属性2','网页换肤','函数接收参数','div变色','鼠标移入改变样式','记住密码提示框'],show:false},
-        {title:'Episode 3',data:['控制div属性3','网页换肤','函数接收参数','div变色','鼠标移入改变样式','记住密码提示框'],show:false},
-        {title:'Episode 4',data:['控制div属性4','网页换肤','函数接收参数','div变色','鼠标移入改变样式','记住密码提示框'],show:false},
-        {title:'Episode 5',data:['控制div属性5','网页换肤','函数接收参数','div变色','鼠标移入改变样式','记住密码提示框'],show:false},
-        {title:'Episode 6',data:['控制div属性6','网页换肤','函数接收参数','div变色','鼠标移入改变样式','记住密码提示框'],show:false},
-    ];
-    //module.export.list = LL_LIST;
