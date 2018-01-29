e7dd79d35894fb1c49cad8b3f047d28cfb437c5f
Micho3 <18600775043@163.com>
2018-01-29 18:39:00 +0800

    增加resume的相关页面，开始写scoll

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,8 +1,5 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-    'TMPL_EXCEPTION_FILE' => './Public/404/404.html', /*[
-        404 =>  APP_PATH.'/Practice/View/Index/404.html',
-        403 =>  APP_PATH.'404.html',
-    ]*/
+//    'TMPL_EXCEPTION_FILE' => './Public/404/404.html',
 );
